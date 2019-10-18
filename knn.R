792d27ce73ac7cf97d3627ca0ca3a9f117541233
lincolnandriatto <lincoln.andriatto@gmail.com>
2019-10-18 00:55:45 -0300

    Ajuste

--- knn.R
+++ knn.R
@@ -2,19 +2,19 @@ library(MASS)
 library(class)
 #help(biopsy)
 
-biopsyAjust <- na.omit(biopsy)
+biopsyAdjustment <- na.omit(biopsy)
 
-L <- sample(1: nrow(biopsyAjust), round(nrow(biopsyAjust)/3))
+L <- sample(1: nrow(biopsyAdjustment), round(nrow(biopsyAdjustment)/3))
 
-train <- biopsyAjust[-L, 2:10]
+train <- biopsyAdjustment[-L, 2:10]
 
-test <- biopsyAjust[L, 2:10]
+test <- biopsyAdjustment[L, 2:10]
 
-cl <- factor(biopsyAjust[-L, 2:11])
+cl <- factor(biopsyAdjustment[-L, 11])
 
 fit <- knn(train, test, cl, k = 2)
 
-c_matrix = table(fit[1: length(L)], factor(biopsyAjust[L, 11]))
+c_matrix = table(fit[1: length(L)], factor(biopsyAdjustment[L, 11]))
 
 print(c_matrix)
 
